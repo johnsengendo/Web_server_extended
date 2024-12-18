3d6db12ab9754a03403fab632909b3b696ea5537
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-12-18 09:38:08 +0100

    Add files via upload

---
+++ Client/install_packages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Updating package lists
+apt-get update -y
+
+# Installing useful tools
+apt-get install -y --no-install-recommends \
+    bash \
+    python3 \
+    python3-pip \
+    bash-completion \
+    curl \
+    net-tools
+    
+# Install Python packages
+pip install requests
+
+# Installing the packages required e.g for dumping traffic.
+apt-get install -y \
+    tcpdump \
+    nano
+
